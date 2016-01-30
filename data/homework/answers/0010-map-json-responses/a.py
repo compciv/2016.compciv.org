589e1fa63b3570703616aa232a496541d5a9882d
Dan Nguyen <dansonguyen@gmail.com>
2016-01-29 17:12:55 -0800

    hey

--- data/homework/answers/0010-map-json-responses/a.py
+++ data/homework/answers/0010-map-json-responses/a.py
@@ -5,28 +5,39 @@ import requests
 STORAGE_DIR = 'tempdata'
 BASE_FILENAME = 'stanford.json'
 URL_DIR = 'http://www.compciv.org/files/datadumps/apis'
-URL_PATHS = [
+
+# I arrange things this way because I see in the
+# given URLs that the first part of the URL
+# is the same...not only that, I *know*
+# that the subdirectories we want to make can be found
+# in partial URL path itself
+PARTIAL_URL_PATHS = [
     'googlemaps/geocode-stanford.json',
     'mapzen/search-stanford.json'
 ]
 
-# make the directory
 
 
-for u in URL_PATHS:
+for upath in PARTIAL_URL_PATHS:
     print("---")  # just a dividing line to make output easier to read...
-    url = join(URL_DIR, u)
+    url = join(URL_DIR, upath)
     print("Downloading from:", url)
     resp = requests.get(url)
-    sub_dir = u.split('/')[0] # e.g. 'googlemaps' or `mapzen`
+    # the subdirectories, e.g. tempdata/mapzen
+    # can be extracted from each partial URL_PATH
+    # e.g. mapzen/search-stanford.json
+    sub_dir = upath.split('/')[0]
     big_dir = join(STORAGE_DIR, sub_dir) # e.g. tempdata/googlemaps
+    # create the directory, e.g. tempdata/googlemaps
     makedirs(big_dir, exist_ok=True)
     # Finally, the full path
+    # e.g. tempdata/googlemaps.stanford.json
     fname = join(big_dir, BASE_FILENAME)
     print("Writing to:", fname)
-    with open(fname, 'w') as f:
-        txt = resp.text
-        f.write(txt)
-        print("Wrote",
-              len(txt.splitlines()), 'lines and',
-              len(txt), 'characters' )
+    f = open(fname, 'w')
+    txt = resp.text
+    f.write(txt)
+    print("Wrote",
+          len(txt.splitlines()), 'lines and',
+          len(txt), 'characters' )
+    f.close()

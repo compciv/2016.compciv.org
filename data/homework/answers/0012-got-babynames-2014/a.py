4a5568370b9064195ae082e683c561e46b726dc7
Dan Nguyen <dansonguyen@gmail.com>
2016-01-29 17:07:51 -0800

    this or that

--- data/homework/answers/0012-got-babynames-2014/a.py
+++ data/homework/answers/0012-got-babynames-2014/a.py
@@ -7,8 +7,15 @@ URL = 'http://stash.compciv.org/ssa_baby_names/ssa-babynames-nationwide-2014.txt
 FPATH = join(DATADIR, basename(URL))
 
 data = requests.get(URL).text
+
+# Note: this is the "cool" way to open and close files
+# at the end of the block, `f` will automatically be closed
 with open(FPATH, 'w') as f:
     f.write(data)
 
+# Alternatively:
+# f = open(FPATH, 'w')
+# f.write(data)
+# f.close()
 
 print("There are", len(data.splitlines()), 'lines in', FPATH)
